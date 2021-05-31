72cd0f16628a816cc09b0a47bad7511fe259cca0
二牛 <erniu.wzh@alibaba-inc.com>
2021-05-31 19:50:46 +0800

    init

---
+++ src/main/java/com/hhwyz/hhwyz/HhwyzApplication.java
@@ -0,0 +1,13 @@
+package com.hhwyz.hhwyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhwyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhwyzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhwyz/hhwyz/HhwyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhwyz.hhwyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhwyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
