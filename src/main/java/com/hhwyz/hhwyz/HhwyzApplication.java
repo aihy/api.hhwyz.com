6dd468447b0b1148bd5edfb407a60e5d9250a3c5
aihy <wzhy1234@qq.com>
2021-06-01 13:08:21 +0800

    update

--- src/main/java/com/hhwyz/hhwyz/HhwyzApplication.java
+++ src/main/java/com/hhwyz/hhwyz/HhwyzApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HhwyzApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HhwyzApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HhwyzApplication.class, args);
+    }
 
 }
--- src/test/java/com/hhwyz/hhwyz/HhwyzApplicationTests.java
+++ src/test/java/com/hhwyz/hhwyz/HhwyzApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HhwyzApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
